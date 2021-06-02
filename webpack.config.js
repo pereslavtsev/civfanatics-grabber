1baccf2a2cf6b6deb5f83ad699121fa064d241ca
Vitaly Pereslavtsev <dev@pereslavtsev.com>
2021-06-02 23:06:05 +0300

    add typings + ignore files

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,7 @@ module.exports = {
   externals: [nodeExternals()],
   output: {
     path: path.resolve(__dirname, './dist'),
+    filename: 'index.js',
     libraryTarget: 'commonjs',
   },
   plugins: [new CleanWebpackPlugin()],
